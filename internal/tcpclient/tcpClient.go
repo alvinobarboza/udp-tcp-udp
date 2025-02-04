262c7cc02ab05b9008848e46ce71f04fcc1dbf9b
Alvino Barboza <alvinobarboza@gmail.com>
2025-02-04 14:19:25 -0300

    TCP client struct

---
+++ internal/tcpclient/tcpClient.go
@@ -0,0 +1,44 @@
+package tcpclient
+
+import "net"
+
+type TCPClient interface {
+	SendMessage([]byte) ([]byte, error)
+}
+
+func NewTCPClient(servAddr string) (TCPClient, error) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tcpClient{
+		tcpAddr: tcpAddr,
+	}, nil
+}
+
+type tcpClient struct {
+	tcpAddr *net.TCPAddr
+}
+
+func (tcp *tcpClient) SendMessage(datagram []byte) ([]byte, error) {
+	conn, errD := net.DialTCP("tcp", nil, tcp.tcpAddr)
+	if errD != nil {
+		return nil, errD
+	}
+
+	_, err1 := conn.Write(datagram)
+	if err1 != nil {
+		return nil, err1
+	}
+
+	reply := make([]byte, 0)
+
+	_, err2 := conn.Read(reply)
+	if err2 != nil {
+		return nil, err2
+	}
+
+	conn.Close()
+	return reply, nil
+}
