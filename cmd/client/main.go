256f7279bcb8de8c9fd3b479968ae59a4d6a4d1d
Alvino Barboza <alvinobarboza@gmail.com>
2025-02-04 17:45:16 -0300

    Tests UDP listener

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alvinobarboza/udp-tcp-udp/internal/filehandler"
+	"github.com/alvinobarboza/udp-tcp-udp/internal/udp"
+)
 
 func main() {
-	fmt.Println("Hello client")
+	defer fmt.Println("Exit")
+	packetSize := 2000
+	tcpMultiplierBuf := 50
+	tcpPktSend := 40
+
+	file := filehandler.NewFileHandler()
+	file.NewFile("teste.bin")
+
+	udpListener := udp.NewUDPListener(tcpMultiplierBuf, packetSize, tcpPktSend, file)
+
+	if err := udpListener.SetUpListener("Ethernet", "234.50.99.3:6000"); err != nil {
+		panic(err)
+	}
+
+	if errU := udpListener.Listen(); errU != nil {
+		panic(errU)
+	}
 }
