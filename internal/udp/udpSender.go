0a12b7dfbf880ab5ab4c6af9378455c6ec5cc1f5
Alvino Barboza <alvinobarboza@gmail.com>
2025-02-06 16:36:41 -0300

    UDP packet sender

---
+++ internal/udp/udpSender.go
@@ -0,0 +1,48 @@
+package udp
+
+import "net"
+
+type UDPSender interface {
+	Write([]byte, chan error)
+	CloseConn()
+}
+
+func NewUDPSender(local, remote string) (UDPSender, error) {
+	localAddr, err1 := net.ResolveUDPAddr("udp", local)
+	if err1 != nil {
+		return nil, err1
+	}
+
+	remoteAddr, err2 := net.ResolveUDPAddr("udp", remote)
+	if err2 != nil {
+		return nil, err2
+	}
+
+	conn, err3 := net.DialUDP("udp", localAddr, remoteAddr)
+	if err3 != nil {
+		return nil, err3
+	}
+
+	return &udpSender{
+		local:  localAddr,
+		remote: remoteAddr,
+		conn:   conn,
+	}, nil
+}
+
+type udpSender struct {
+	local  *net.UDPAddr
+	remote *net.UDPAddr
+	conn   *net.UDPConn
+}
+
+func (ud *udpSender) Write(data []byte, err chan error) {
+	_, err1 := ud.conn.Write(data)
+	if err1 != nil {
+		err <- err1
+	}
+}
+
+func (us *udpSender) CloseConn() {
+	us.conn.Close()
+}
