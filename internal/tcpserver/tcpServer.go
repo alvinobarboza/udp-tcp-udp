7ee4be7ddafe9cf0ffe41a46650ae0ca5ceffcc1
Alvino Barboza <alvinobarboza@gmail.com>
2025-02-12 15:32:45 -0300

    Read header for size and counter as well default close and early close connection

--- internal/tcpserver/tcpServer.go
+++ internal/tcpserver/tcpServer.go
@@ -1,11 +1,11 @@
 package tcpserver
 
 import (
-	"bufio"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
-	"time"
+	"sync"
 
 	"github.com/alvinobarboza/udp-tcp-udp/internal/filehandler"
 )
@@ -15,9 +15,11 @@ type TCPServer interface {
 }
 
 type tcpServer struct {
-	udp    UDPSender
-	file   filehandler.FileHandler
-	ipAddr string
+	udp     UDPSender
+	file    filehandler.FileHandler
+	ipAddr  string
+	counter uint32
+	mu      sync.Mutex
 }
 
 func NewTCPServer(ipaddr string, udp UDPSender, file filehandler.FileHandler) TCPServer {
@@ -49,23 +51,64 @@ func (ts *tcpServer) Listen() error {
 	}
 }
 
-func (ts *tcpServer) handlRequest(conn net.Conn, err chan error) {
+func (ts *tcpServer) handlRequest(conn net.Conn) {
 	defer conn.Close()
+	ts.mu.Lock()
+	ts.counter++
+	local := ts.counter
+	ts.mu.Unlock()
 
-	conn.SetReadDeadline(time.Now().Add(time.Second * 3))
+	header := make([]byte, 5)
+	_, errH := conn.Read(header)
 
-	reader := bufio.NewReader(conn)
-	body := make([]byte, 1500)
-	read, errM := reader.Read(body)
-
-	if errM != nil {
-		conn.Close()
-		err <- errM
+	if errH != nil {
+		log.Println(errH, local, header)
 		return
 	}
-	fmt.Printf("Size: %02d body: %02d\n", read, body[:read])
+
+	if eofFromClient(header) {
+		log.Println("Closed early by client", local)
+		return
+	}
+
+	pCount := binary.LittleEndian.Uint16(header[0:2])
+	pSize := binary.LittleEndian.Uint16(header[2:4])
+	fmt.Printf("\nSize: %02d counter: %02d - %d\n", pSize, pCount, header)
+
+	data := make([]byte, pSize)
+	for {
+		dRead, errR := conn.Read(data)
+
+		if errR != nil {
+			log.Println(errR, "req: ", pCount)
+			return
+		}
+		if dRead == 5 && eofFromClient(data) {
+			log.Println("Closed early by client", local, pCount)
+			return
+		}
+		if dRead == 2 {
+			fmt.Println("\nClosed", data[:dRead])
+			break
+		}
+		fmt.Printf("read: %02d\r", dRead)
+	}
 
 	// ts.pktWriter.Write(body[0:read], err)
 
 	conn.Write([]byte("Received"))
+}
+
+func eofFromClient(data []byte) bool {
+	checkBound := 5
+	counter := 0
+	for i, d := range data {
+		if i > checkBound {
+			break
+		}
+		if d == 0xff {
+			counter++
+		}
+	}
+	return counter == checkBound
 }
